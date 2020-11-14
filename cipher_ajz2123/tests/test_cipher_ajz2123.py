bd412ad8755613b083b8fc860efa026ab0d9c184
Alys-217 <ajz2123@tc.columbia.edu>
2020-11-13 22:39:48 -0500

    add cipher test

--- cipher_ajz2123/tests/test_cipher_ajz2123.py
+++ cipher_ajz2123/tests/test_cipher_ajz2123.py
@@ -3,3 +3,10 @@ from cipher_ajz2123 import cipher_ajz2123
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_cipher():
+    example = 'abc'
+    shift = 5
+    expected = 'def'
+    actual = cipher_ajz2123.cipher(example, shift)
+    assert actual == expected
