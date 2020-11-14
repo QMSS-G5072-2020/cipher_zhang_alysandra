e607383c5236d3e47f44293be53f7f1445198eb4
Alys-217 <ajz2123@tc.columbia.edu>
2020-11-13 22:04:07 -0500

    add docstrings to cipher function

--- cipher_ajz2123/cipher_ajz2123/cipher_ajz2123.py
+++ cipher_ajz2123/cipher_ajz2123/cipher_ajz2123.py
@@ -1,4 +1,28 @@
-def cipher(text, shift, encrypt=True):    
+def cipher(text, shift, encrypt=True):
+    """
+    This is a function used to encipher or decipher keyboard text input using the Casesar cipher.
+
+    Inputs:
+    (1) Text: alphabetical strings to encrypt or decrypt;
+    (2) Shift: integer input to replace plaintext letter with a different one with fixed number of places up or down the alphabet;
+    (3) Encrypt: Boolean value (e.g., True, False);
+        If the encrypt parameter is set to True, the text will be encrypted;
+        If the encrypt parameter is set to False, the text will be decrypted.
+
+    Output:
+    The output is text that shows the result of the encipher or decipher.
+
+    Examples:
+    
+    from cipher_ajz2123 import cipher_ajz2123
+
+    cipher('abc', 5, True)
+    'fgh'
+
+    cipher('abc', 5, False)
+    'VWX'
+
+    """
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
