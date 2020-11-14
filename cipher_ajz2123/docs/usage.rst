63140682067f0a8a4c2fb95c9a5ae78fd6e04967
Alys-217 <ajz2123@tc.columbia.edu>
2020-11-13 21:52:39 -0500

    initial commit

---
+++ cipher_ajz2123/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use cipher_ajz2123 in a project::
+
+    import cipher_ajz2123
---
+++ cipher_ajz2123/tests/test_cipher_ajz2123.py
@@ -0,0 +1,5 @@
+from cipher_ajz2123 import __version__
+from cipher_ajz2123 import cipher_ajz2123
+
+def test_version():
+    assert __version__ == '0.1.0'
